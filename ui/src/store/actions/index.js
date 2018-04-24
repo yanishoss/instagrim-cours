2844912e80576b2f4f8c4baa2f757b5b8264dca5
Yanis HEDJEM <yanishproduction@gmail.com>
2018-04-24 06:53:49 +0200

    Cleaned the projet for the course purpose

--- ui/src/store/actions/index.js
+++
@@ -1,7 +0,0 @@
-import * as types from './types';
-
-export {
-    searchDoQuery,
-    searchSetResults
-} from './search.js';
-export { types };
--- ui/src/store/actions/types.js
+++
@@ -1,2 +0,0 @@
-export const SEARCH_DO_QUERY = 'SEARCH_DO_QUERY';
-export const SEARCH_SET_RESULTS = 'SEARCH_SET_RESULTS';
