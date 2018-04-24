633987c9e5a7b0f5fde1555a909280d021234ca1
Yanis HEDJEM <yanishproduction@gmail.com>
2018-04-24 13:46:30 +0200

    Ajout de la structure du projet

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,28 +1,19 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
-
-import configureStore from './store/store';
-import Layout from './containers/Layout/Layout';
-
-const store = configureStore();
-
-const client = new ApolloClient({
-  uri: 'http://localhost:8080/graphql'
-})
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <ApolloProvider client={client}>
-          <BrowserRouter>
-            <Layout />
-          </BrowserRouter>
-        </ApolloProvider>
-      </Provider>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
     );
   }
 }
--- ui/src/index.js
+++ ui/src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js');
-}
-
-ReactDOM.render( <App /> , document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
