fa686cd3e80f00544b7c9d64227e29ccd6d1f68f
Yanis HEDJEM <yanishproduction@gmail.com>
2018-05-03 12:45:13 +0200

    Finished Validator Lib

--- ui/src/utils/validation/validation.js
+++ ui/src/utils/validation/validation.js
@@ -1,59 +1,138 @@
+import lodash from 'lodash';
+
 // State of the class
 let state = {
-    typeToValidatingFunctions: new Map()
+    mapTypesToHandlers: new Map(),
+    mapParametersToHandlers: new Map()
 };
 
+const applyParameters = (element, parameters, mapParametersToHandlers) => {
+    let parametersResults = [];
+    for (let parameter in parameters) {
+        const parameterHandlers = mapParametersToHandlers.get(parameter)
+            ? mapParametersToHandlers.get(parameter)
+            : [];
+        const parameterResults = parameterHandlers.map(func => func(element, parameters[parameter]));
+        parametersResults = [
+            ...parametersResults,
+            ...parameterResults
+        ];
+    }
+    return parametersResults.every(valid => valid === true);
+}
+
+const applyTypes = (element, types, mapTypesToHandlers) => {
+    const typesArray = types
+        .trim()
+        .split(",");
+    let typesResults = [];
+    for (let type of typesArray) {
+        const typeHandlers = mapTypesToHandlers.get(type)
+            ? mapTypesToHandlers.get(type)
+            : [];
+        const typeResults = typeHandlers.map(func => func(element));
+        typesResults = [
+            ...typesResults,
+            ...typeResults
+        ];
+    }
+    return typesResults.every(valid => valid === true);
+}
+
 // Is a singleton class
-export default class Validator {
+class Validator {
     constructor() {}
 
-    // The validating function is a function that takes the element to validate and
-    // return a boolean
+    // The type handler is a function that takes the element to validate and return
+    // a boolean
 
-    static addValidationType(name, validatingFunction) {
+    static addType(name, typeHandler) {
         // It checks whether the name provided already exists, if so returns the array
         // of validating functions associated else just create an empty array
-        const existingValidatingFunctions = state
-            .typeToValidatingFunctions
+        const existingTypeHandlers = state
+            .mapTypesToHandlers
             .get(name)
             ? state
-                .typeToValidatingFunctions
+                .mapTypesToHandlers
                 .get(name)
             : [];
 
         state
-            .typeToValidatingFunctions
+            .mapTypesToHandlers
             .set(name, [
-                ...existingValidatingFunctions,
-                validatingFunction
+                ...existingTypeHandlers,
+                typeHandler
             ]);
 
         return;
     }
 
-    static deleteValidationType(name, validatingFunction = null) {
+    static deleteType(name, typeHandler = null) {
         // Warning, you should never check the arguments of a function but, in my case,
         // my function got 2 duties. The first is delete the provided function of the
         // type. The second is delete the type completely if not function is provided.
         // If provided a function to delete, it delete that, in the else case, just
         // deletes the type completely
-        if (validatingFunction) {
+        if (typeHandler) {
             // Get the existing functions and get rid of the function to delete
-            const newValidatingFunctions = state
-                .typeToValidatingFunctions
+            const newTypeHandlers = state
+                .mapTypesToHandlers
                 .get(name)
-                .filter(func => func === validatingFunction);
+                .filter(func => func.toString() !== typeHandler.toString());
 
             state
-                .typeToValidatingFunctions
-                .set(name, newValidatingFunctions);
+                .mapTypesToHandlers
+                .set(name, newTypeHandlers);
 
             return;
         }
 
         // Get rid of the type completely
         state
-            .typeToValidatingFunctions
+            .mapTypesToHandlers
+            .delete(name);
+
+        return;
+    }
+
+    // Parameter Handler is a function: (element, valueOfParameter) => {  do
+    // something...  return true or false; }
+    static addParameter(name, parameterHandler) {
+        const existingParameterHandlers = state
+            .mapParametersToHandlers
+            .get(name)
+            ? state
+                .mapParametersToHandlers
+                .get(name)
+            : [];
+
+        state
+            .mapParametersToHandlers
+            .set(name, [
+                ...existingParameterHandlers,
+                parameterHandler
+            ]);
+        return;
+    }
+
+    static deleteParameter(name, parameterHandler = null) {
+        if (parameterHandler) {
+            // Get the existing functions and get rid of the function to delete
+            const newParameterHandlers = state
+                .mapParametersToHandlers
+                .get(name)
+                .filter(func => func.toString() !== parameterHandler.toString());
+
+            state
+                .mapParametersToHandlers
+                .set(name, newParameterHandlers);
+
+            return;
+        }
+
+        // Get rid of the type completely
+        state
+            .mapParametersToHandlers
             .delete(name);
 
         return;
@@ -61,17 +140,58 @@ export default class Validator {
 
     // Checks if provided element is valid according to the type's validating
     // functions
-    validate(name, element) {
-        const validatingFunctions = state
-            .typeToValidatingFunctions
-            .get(name);
-
-        if (!validatingFunctions) {
-            return true;
-        }
-
-        const isValid = validatingFunctions.every(func => func(element));
-
-        return isValid;
+    validate(element, validationConfig) {
+        const typesAreValid = validationConfig.types
+            ? applyTypes(element, validationConfig.types, state.mapTypesToHandlers)
+            : true;
+        const parametersAreValid = applyParameters(element, validationConfig, state.mapParametersToHandlers);
+        return typesAreValid && parametersAreValid;
     }
 };
+
+Validator.addParameter("min", (element, valueOfParameter) => {
+    switch (typeof element) {
+        case "string":
+            return element.length >= valueOfParameter;
+        case "number":
+            return element >= valueOfParameter;
+        case "object":
+            if (Array.isArray(element)) {
+                return element.length >= valueOfParameter;
+            }
+            return lodash.size(element) >= valueOfParameter;
+        default:
+            return true;
+    }
+});
+
+Validator.addParameter("max", (element, valueOfParameter) => {
+    switch (typeof element) {
+        case "string":
+            return element.length <= valueOfParameter;
+        case "number":
+            return element <= valueOfParameter;
+        case "object":
+            if (Array.isArray(element)) {
+                return element.length <= valueOfParameter;
+            }
+            return lodash.size(element) <= valueOfParameter;
+        default:
+            return true;
+    }
+});
+
+Validator.addParameter("equal", (element, valueOfParameter) => element == valueOfParameter);
+Validator.addParameter("deepEqual", (element, valueOfParameter) => lodash.isEqual(element, valueOfParameter));
+
+Validator.addType("email", element => {
+    const emailRegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegExp.test(element);
+});
+
+Validator.addType("phone-fr", element => {
+    const phoneRegExp = /^((\+)33|0)[1-9](\d{2}){4}$/g;
+    return phoneRegExp.test(element);
+});
+
+export default Validator;
