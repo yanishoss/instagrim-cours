bad69f14a9a1a54f6ddc29b14dcc48929f3cde94
Yanis HEDJEM <yanishproduction@gmail.com>
2018-05-09 00:06:37 +0200

    Finished the signup page

--- ui/src/pages/SignUp/SignUp.js
+++ ui/src/pages/SignUp/SignUp.js
@@ -1,11 +1,19 @@
-import React, {Component} from "react";
-import {TextField, Button} from "material-ui";
+import React, { Component } from "react";
 import styled from "styled-components";
-import gql from 'graphql-tag';
-import {Mutation} from 'react-apollo';
+import { Mutation } from "react-apollo";
+import { TextField, Button } from "material-ui";
+import gql from "graphql-tag";
 
-import FormValidator from "./../../utils/Validator/FormValidator";
-import Validate from "./../../utils/Validator/Validate";
+import FormValidator from './../../utils/Validator/FormValidator';
+import Validate from './../../utils/Validator/Validate';
+
+const CREATE_USER = gql`
+    mutation createUser($username: String!, $email: String!, $password: String!){
+        createUser(username: $username, email: $email, password: $password){
+            id
+        }
+    }
+`;
 
 const formConfiguration = {
     email: {
@@ -14,7 +22,7 @@ const formConfiguration = {
     username: {
         types: "required",
         min: 5,
-        max: 8
+        max: 10
     },
     password: {
         types: "required",
@@ -22,14 +30,6 @@ const formConfiguration = {
         max: 60
     }
 };
-
-const CREATE_USER = gql`
-    mutation createUser($username: String!, $email: String!, $password: String!) {
-        createUser(username: $username, email: $email, password: $password) {
-            id
-        }
-    }
-`;
 
 const StyledDiv = styled.div`
     display: flex;
@@ -91,37 +91,37 @@ export default class SignUp extends Component {
         form: {
             validation: {
                 fields: {
-                    email: false,
                     username: false,
+                    email: false,
                     password: false
                 },
                 valid: false
             },
-            email: "",
             username: "",
+            email: "",
             password: ""
         }
     }
 
-    setInputValue = (name, value) => {
-        this.setState(prevState => ({
+    setValidation = newValidationState => {
+        this.setState({
             form: {
-                ...prevState.form,
+                ...this.state.form,
+                validation: newValidationState
+            }
+        });
+    }
+
+    setFieldValue = (name, value) => {
+        this.setState({
+            form: {
+                ...this.state.form,
                 [name]: value
             }
-        }));
+        });
     }
 
-    setFormValidation = state => {
-        this.setState(prevState => ({
-            form: {
-                ...prevState.form,
-                validation: state
-            }
-        }));
-    }
-
-    sendForm = (createUser, e) => {
+    handleSubmit = (createUser, e) => {
         e.preventDefault();
 
         if (!this.state.form.validation.valid){
@@ -132,66 +132,70 @@ export default class SignUp extends Component {
 
         createUser({
             variables: {
-                username,
                 email,
+                username,
                 password
             }
-        })
+        });
     }
 
 	render() {
         const {email, username, password} = this.state.form;
+        const {setValidation, setFieldValue, handleSubmit} = this;
         const emailIsNotValid = !this.state.form.validation.fields.email && Boolean(email);
         const usernameIsNotValid = !this.state.form.validation.fields.username && Boolean(username);
         const passwordIsNotValid = !this.state.form.validation.fields.password && Boolean(password);
-        const {setFormValidation, sendForm, setInputValue} = this;
 		return (
             <Mutation mutation={CREATE_USER}>
-                {(createUser, {loading, error, data}) => (
+                {(createUser, {data, loading, error}) => (
                     <StyledDiv>
-                        <FormValidator configuration={formConfiguration} getState={setFormValidation}>
-                            <h1>Inscrivez-vous, parce que c'est cool !</h1>
-                            <form onSubmit={e => sendForm(createUser, e)}>
+                        <h1>Inscrivez-vous, parce que c'est cool !</h1>
+                        <FormValidator 
+                            configuration={formConfiguration} 
+                            getState={setValidation}>
+                            <form onSubmit={e => handleSubmit(createUser, e)}>
                                 <Validate name="email">
                                     <TextField 
-                                        label="Email"
+                                        label="Adresse mail"
                                         placeholder="Entrez votre adresse mail"
-                                        inputProps={{
-                                            type: "email"
-                                        }}
-                                        onChange={e => setInputValue("email", e.target.value)}
-                                        value={email}
                                         error={emailIsNotValid}
-                                        fullWidth
-                                        required/>
+                                        onChange={e => setFieldValue("email", e.target.value)}
+                                        value={email}
+                                        required
+                                        fullWidth/>
                                 </Validate>
                                 <Validate name="username">
                                     <TextField 
                                         label="Nom d'utilisateur"
                                         placeholder="Choisissez un nom d'utilisateur"
-                                        onChange={e => setInputValue("username", e.target.value)}
-                                        value={username}
                                         error={usernameIsNotValid}
-                                        fullWidth
-                                        required />
+                                        onChange={e => setFieldValue("username", e.target.value)}
+                                        value={username}
+                                        required
+                                        fullWidth/>
                                 </Validate>
                                 <Validate name="password">
                                     <TextField 
                                         label="Mot de passe"
                                         placeholder="Imaginez un mot de passe"
-                                        inputProps={{
-                                            type: "password"
-                                        }}
-                                        onChange={e => setInputValue("password", e.target.value)}
-                                        value={password}
                                         error={passwordIsNotValid}
-                                        
-                                        fullWidth
-                                        required />
+                                        onChange={e => setFieldValue("password", e.target.value)}
+                                        value={password}
+                                        type="password"
+                                        required
+                                        fullWidth/>
                                 </Validate>
-                                {!data && <Button variant="raised" size="large" color="primary" type="submit">S'inscrire</Button>}
+                                {!data && (
+                                    <Button
+                                        variant="raised"
+                                        size="large"
+                                        color="primary"
+                                        type="submit">
+                                        S'inscrire
+                                    </Button>
+                                )}
                                 {data && <p>Bienvenue dans la confrérie, connectes-toi et on te trouvera des amis !</p>}
-                                {loading && <p>Chuuuuut, ça charge...</p>}
+                                {loading && <p>Chuuuut, ça charge...</p>}
                                 {error && <p>Oups, tu as essayé d'usurper l'identité d'un autre utilisateur, essaie encore...</p>}
                             </form>
                         </FormValidator>
